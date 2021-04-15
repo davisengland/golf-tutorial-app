aeb28c16e452e2412a016b17d37bd8478fa724f1
davisengland <davisrodneyengland@gmail.com>
2021-04-15 15:42:13 -0600

    working on adding and pulling data to and from s3, then rendering that data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const aws = require('aws-sdk')
 const express = require('express')
 const app = express()
 const massive = require('massive')
@@ -6,8 +7,9 @@ const session = require('express-session')
 const usersCtrl = require('./controllers/usersController')
 const tutorialsCtrl = require('./controllers/tutorialsController')
 const historyCtrl = require('./controllers/historyController')
+const { default: axios } = require('axios')
 
-const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env
 
 app.use(express.json())
 app.use(session({
@@ -29,6 +31,37 @@ app.get('/tutorials/:id', tutorialsCtrl.getTutorial)
 app.post('/tutorials', tutorialsCtrl.addTutorial)
 app.get('/history', historyCtrl.getHistory)
 app.post('/history', historyCtrl.addToHistory)
+app.get('/sign-s3', (req, res) => {
+    aws.config = {
+        region: 'us-west-1',
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY
+    }
+
+    const s3 = new aws.S3()
+    const fileName = req.query['file-name']
+    const fileType = req.query['file-type']
+    const folderName = req.query['folder-name']
+    const s3Params = {
+        Bucket: S3_BUCKET,
+        Key: `${folderName}/${fileName}`,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'public-read'
+    }
+
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+        if(err) {
+            console.log(err)
+            return res.end()
+        }
+        const returnData = {
+            signedRequest: data,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${folderName}/${fileName}`
+        }
+        return res.send(returnData)
+    })
+})
 
 massive({
     connectionString: CONNECTION_STRING,
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,19 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import { connect } from "react-redux";
 import YouTube from "react-youtube";
+import Dropzone from 'react-dropzone'
+import { GridLoader } from 'react-spinners'
+import { v4 as randomString } from 'uuid'
+import ReactPlayer from 'react-player'
 import './Profile.css'
+import axios from "axios";
 
 function Profile(props) {
-  navigator.webkitPersistentStorage.YTConfig = { host: 'https://www.youtube.com' } 
+  const [isUploading, setIsUploading] = useState(false)
+  const [url, setUrl] = useState('http://via.placeholder.com/450x450')
+
+  navigator.webkitPersistentStorage.YTConfig = { host: 'https://www.youtube.com' }
 
   const historyMap = props.historyReducer.history.map((elem) => {
     return (
@@ -19,6 +27,48 @@ function Profile(props) {
     );
   });
 
+  const getSignedRequest = ([file]) => {
+    setIsUploading(true)
+ 
+    const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`
+    const folderName = props.userReducer.user.user_id
+ 
+    axios.get('/sign-s3', {
+      params: {
+        'file-name': fileName,
+        'file-type': file.type,
+        'folder-name': folderName
+      }
+    }).then( (response) => {
+      const { signedRequest, url } = response.data
+      uploadFile(file, signedRequest, url)
+    }).catch( err => {
+      console.log(err)
+    })
+  }
+
+  const uploadFile = (file, signedRequest, url) => {
+    const options = {
+      headers: {
+        'Content-Type': file.type
+      }
+    }
+
+    axios.put(signedRequest, file, options)
+      .then(res => {
+        setIsUploading(false)
+        setUrl(url)
+      })
+      .catch(err => {
+        setIsUploading(false)
+        if(err.response.status === 403) {
+          alert(`ERROR:\n${err.stack}`)
+        } else {
+          alert(`ERROR: ${err.status}\n ${err.stack}`)
+        }
+      })
+  }
+
   return (
     <div className='profile-page'>
       <Header />
@@ -28,17 +78,42 @@ function Profile(props) {
           {props.userReducer.user.last_name?.charAt(0).toUpperCase()}
         </h1>
         <div className='user-info'>
-          <p className='info-descriptions'>
+          <div className='info-descriptions'>
             <h2>First Name:</h2>
             <h2>Last Name:</h2>
             <h2>Email:</h2>
-          </p>
-          <p className='info'>
+          </div>
+          <div className='info'>
             <h2>{props.userReducer.user.first_name}</h2>
             <h2>{props.userReducer.user.last_name}</h2>
             <h2>{props.userReducer.user.email}</h2>
-          </p>
+          </div>
         </div>
+        <h1>Upload Practice Video</h1>
+        <Dropzone
+          onDropAccepted={getSignedRequest}
+          accept="video/*"
+          multiple={false}>
+          {({getRootProps, getInputProps}) => (
+          <div 
+              style = {{
+              width: 160,
+              height: 80,
+              borderWidth: 5,
+              marginTop: 25,
+              borderColor: 'gray',
+              borderStyle: 'dashed',
+              borderRadius: 5,
+              display: 'inline-block',
+              fontSize: 17,}}
+              {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isUploading ? <GridLoader /> : <p>Drop files here, or click to select files</p>}
+          </div>
+          )}
+        </Dropzone>
+        <ReactPlayer url='https://golf-tutorials.s3-us-west-1.amazonaws.com/6/c751fc26-b5c9-4909-b6b4-b5331c69c285-IMG_0021.MOV'/>
+        <ReactPlayer url='https://golf-tutorials.s3-us-west-1.amazonaws.com/1/569f74cd-aa06-4d53-a981-2ea657b96786-IMG_0021.mp4'/>
         <h1>History</h1>
         {historyMap}
       </section>
